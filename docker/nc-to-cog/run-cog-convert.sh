e3cf5b7dbcd001f469fc369aa64eb959c0ea5c8d
Aimee Barciauskas <aimee@developmentseed.org>
2020-05-12 17:06:36 -0700

    Fix for AWS S3 upload

--- docker/nc-to-cog/run-cog-convert.sh
+++ docker/nc-to-cog/run-cog-convert.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# S3 bucket and path to read urls from and write COGs to
-# TODO example shows filepath as initial input
-AWS_S3_PATH=$1
-
-# Determine which URL to fetch
+# RUN IN BATCH
 if [[ -n $AWS_BATCH_JOB_ARRAY_INDEX ]]
 then
+  # S3 bucket and path to read urls from and write COGs to
+  # TODO example shows filepath as initial input
+  AWS_S3_PATH=$1
+
+  # Get list of urls
+  aws s3 cp $AWS_S3_PATH/urls.txt .
+
   LINE_NUMBER=$(($AWS_BATCH_JOB_ARRAY_INDEX + 1))
   SRC_URL=`sed -n ''$LINE_NUMBER','$LINE_NUMBER'p' < urls.txt`
 elif [[ -n $1 ]]
 then
-  # In case we pass a specific url
+  # Run for a specific url
   SRC_URL=$1
 else
   echo 'No url parameter, please pass a URL for testing'
@@ -19,12 +22,14 @@ else
 fi
 wget $SRC_URL
 
-# Get list of urls
-aws s3 cp s3://$AWS_S3_PATH/urls.txt .
-
 FILENAME=`url="${SRC_URL}"; echo "${url##*/}"`
 echo 'Generating COG from '$FILENAME
 
 python3 /home/handler.py -f $FILENAME
 
-aws s3 cp ${FILENAME}.cog.tif $AWS_S3_COG_LOCATION
+if [[ -n $AWS_S3_PATH ]]
+then
+  echo "Writing ${FILENAME}.cog.tif to $AWS_S3_PATH"
+  aws s3 cp ${FILENAME}.cog.tif $AWS_S3_PATH/${FILENAME}.cog.tif
+fi
+
