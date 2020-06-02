0253c8597860099c58b08394edd166e832dccc8e
Aimee Barciauskas <aimee@developmentseed.org>
2020-06-01 18:25:31 -0700

    Provide example for looping through years

--- docker/list-urls/run.sh
+++ docker/list-urls/run.sh
@@ -4,6 +4,16 @@
 #   https://acdisc.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level3/OMNO2d.003/2020/ \
 #   3 \
 #   s3://omi-no2-nasa/validation/urls.txt
+# or for appending:
+# 
+# for year in 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016
+# do
+#   ./run.sh he5\" \
+#     https://acdisc.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level3/OMNO2d.003/${year}/ \
+#     3 \
+#     s3://omi-no2-nasa/urls.txt
+# done
+#
 # ./run.sh 2020 \
 #   https://e4ftl01.cr.usgs.gov/MOTA/MCD19A2.006/ \
 #   5 \
@@ -23,7 +33,7 @@ wget -e robots=off --force-html -O - $PARENT_DIRECTORY | \
   grep ${LINK_FINDER} | awk '{ print $'$AWK_ARG' }' | sed -e 's/.*href=['"'"'"]//' -e 's/["'"'"'].*$//' > filenames.txt
 
 # Form complete urls from parent directory and filename
-cat filenames.txt | while read line; do echo ${PARENT_DIRECTORY}$line ; done > urls.txt
+cat filenames.txt | while read line; do echo ${PARENT_DIRECTORY}$line ; done >> urls.txt
 
 echo "Done generating urls.txt"
 aws2 s3 cp urls.txt $AWS_S3_PATH --acl public-read
