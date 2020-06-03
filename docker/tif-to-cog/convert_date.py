7c6e1b32023d3d0620cf5fcdabe4b78f13c99ebf
Aimee Barciauskas <aimee@developmentseed.org>
2020-06-03 10:12:03 -0700

    Fix filename date time

--- docker/tif-to-cog/convert_date.py
+++ docker/tif-to-cog/convert_date.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 filename = args.filename
 monthyear = filename.split('_')[-2].split('.')[0]
 month_format = "%B" if 'April' in monthyear or 'March' in monthyear else "%b"
-formatted_date = datetime.datetime.strptime("{0}".format(monthyear), f"{month_format}%Y").strftime("%Y_%m_%d")
+formatted_date = datetime.datetime.strptime("{0}".format(monthyear), f"{month_format}%Y").strftime("%Y%m")
 new_filename = filename.replace(monthyear, formatted_date)
 os.rename(filename, new_filename)
 print(new_filename)
