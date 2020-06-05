159994a0924d81bd38b0008c3af35a3c9057780a
Aimee Barciauskas <aimee@developmentseed.org>
2020-06-05 10:12:53 -0700

    Add comments to script

--- scripts/reconcileFiles.js
+++ scripts/reconcileFiles.js
@@ -1,4 +1,12 @@
-s3Files = fs.readFileSync('', 'utf-8').split('\n')
+// Example script for reconciling any files which might be missing from S3
+// To use, first generate lists of:
+// `s3Files` - list of files on S3 (this currently expects S3 files to be .tif files)
+// `s3Urls` - list of expected files (this expects URLs to .he5 files)
+// Modify as needed
+//
+// Use the output, missing-urls.txt, in a job (e.g. AWS Batch or AWS Lambda) to
+// process missing files.
+s3Files = fs.readFileSync('s3-files.txt', 'utf-8').split('\n')
 s3Urls = fs.readFileSync('', 'utf-8').split('\n')
 missingFiles = [];
 s3Urls.forEach(url => {
@@ -6,4 +14,4 @@ s3Urls.forEach(url => {
     missingFiles.push(url)
   }
 });
-fs.writeFileSync('missing-omi-urls.txt', missingFiles.join('\n'));
+fs.writeFileSync('missing-urls.txt', missingFiles.join('\n'));
