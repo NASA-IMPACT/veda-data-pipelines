c096e3ad82ea67228196c2ce200a4e7178349d53
Aimee Barciauskas <aimee@developmentseed.org>
2020-06-05 10:04:34 -0700

    Cleanrup comments in collection helpers

--- docker/hdf4-to-cog/collection_helpers.py
+++ docker/hdf4-to-cog/collection_helpers.py
@@ -7,18 +7,17 @@ def select_from_orbits(args, hdf_file, data_var):
     Given a data var is part of a subdataset with multiple orbits, select data
     from the orbit which had the minimum angle for `orbit_sds_name`.
     """
+    # Scale the orbit data (5km for MODIS AOD) to the same spatial scale as the data (1km).
     orbit_data = hdf_file.select(args['orbit_sds_name'])
-    # First dimension is the number of orbits
-    # To get the scale factor
     orbit_height = orbit_data.dim(1).length()
     orbit_width = orbit_data.dim(2).length()
-    # TODO: Not DRY - we recalculate src_width and src_height here
+    # FIXME: DRY - Recalculating src_width and src_height which is already
+    # done in handler.py
     src_width = data_var.dim(1).length()
     src_height = data_var.dim(2).length()    
     upscale_height_factor = src_height / orbit_height
     upscale_width_factor = src_width / orbit_width
 
-    # num_orbits x src_height x src_width grid
     angle_scale_factor = orbit_data.attributes()["scale_factor"]
     angle_nodata = orbit_data.getfillvalue()
     def mycos(v):
@@ -30,7 +29,8 @@ def select_from_orbits(args, hdf_file, data_var):
             return angle_nodata
 
     angles = np.vectorize(mycos)(orbit_data[:])
-    # Fix me!!! ignore any angles which are invalid (nodata values)
+    # FIXME: This is a workaround nodata values.
+    # Set nodata values to a large value so they are not a part of the orbit selection algorithm
     angles[angles == angle_nodata] = 1000
     orbit_resampled = scipy.ndimage.zoom(
         angles,
