245796d85de2e69fcb177566cf30aec5c5a97fc3
Aimee Barciauskas <aimee@developmentseed.org>
2020-06-08 10:29:10 -0700

    Blackmarble Code (#27)
    * rough draft convert envi .img file to cog
    
    * Add tif-to-cog
    
    * docker images and configuration for blackmarble nightlights
    
    * Fix filename
    
    * Add file renaming
    
    * Add convert date file
    
    * Fix date / filenaming
    
    * Fix filename date time
    
    * Organize .gitignore
    
    * Don't need netCDF4
    
    * Oh wait we are using netCDF4 but in a different handler
    
    * Update documentation
    
    * Modify commands and comments in docker/list-urls/run-s3.sh
    
    * Update README for list urls
    
    * Add list-s3-files.sh
    
    * Remove duplicate terraform declarations
    
    * Update README
    
    Co-authored-by: Rachel Wegener <rachel.e.wegener@gmail.com>

---
+++ docker/tif-to-cog/convert_date.py
@@ -0,0 +1,15 @@
+import datetime
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Generate COG from .img file and schema")
+parser.add_argument("-f", "--filename", help="filename to convert")
+args = parser.parse_args()
+
+filename = args.filename
+monthyear = filename.split('_')[-2].split('.')[0]
+month_format = "%B" if 'April' in monthyear or 'March' in monthyear else "%b"
+formatted_date = datetime.datetime.strptime("{0}".format(monthyear), f"{month_format}%Y").strftime("%Y%m")
+new_filename = filename.replace(monthyear, formatted_date)
+os.rename(filename, new_filename)
+print(new_filename)
