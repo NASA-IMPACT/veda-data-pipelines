ce7452d2d932f6420b4e3de36a14075382c2cbf2
Aimee Barciauskas <aimee@developmentseed.org>
2020-09-10 15:46:33 -0700

    Add github action and new dockerfile

---
+++ docker/hdf5-to-cog/run.py
@@ -0,0 +1,73 @@
+from netCDF4 import Dataset
+from affine import Affine
+from rasterio.crs import CRS
+from rasterio.io import MemoryFile
+from rio_cogeo.cogeo import cog_translate
+from rio_cogeo.profiles import cog_profiles
+import numpy as np
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Generate COG from file and schema")
+parser.add_argument("-f", "--filename", help="HDF5 or NetCDF filename to convert")
+args = parser.parse_args()
+
+# input file schema
+f1 = dict(
+    src_path=args.filename,
+    group="Grid",
+    variable_name="precipitationCal"
+)
+
+# Set COG inputs
+output_profile = cog_profiles.get(
+    "deflate"
+)  # if the files aren't uint8, this will need to be changed
+output_profile["blockxsize"] = 256
+output_profile["blockysize"] = 256
+
+def to_cog(
+        src_path: str,
+        group: str,
+        variable_name: str):
+    """HDF5 to COG."""
+    # Open existing dataset
+    src = Dataset(src_path, "r")
+    variable = src.groups[group][variable_name][:]
+    xmin, ymin, xmax, ymax = [-180, -90, 180, 90]
+    nrows, ncols = variable.shape[0], variable.shape[1]
+    print("nrows, ncols: ", nrows, ncols)
+    xres = (xmax - xmin) / float(nrows)
+    yres = (ymax - ymin) / float(ncols)
+    geotransform = (xmin, xres, 0, ymax, 0, yres)
+    dst_transform = Affine.from_gdal(*geotransform)
+    nodata_value = variable.fill_value
+
+    # Save output as COG
+    output_profile = dict(
+        driver="GTiff",
+        dtype=variable.dtype,
+        count=1,
+        # not sure this is correct, we need to flip things around
+        height=ncols,
+        width=nrows,
+        crs=CRS.from_epsg(4326),
+        transform=dst_transform,
+        nodata=nodata_value,
+        tiled=True,
+        compress="deflate",
+        blockxsize=256,
+        blockysize=256,
+    )
+    print("profile h/w: ", output_profile["height"], output_profile["width"])
+    with MemoryFile() as memfile:
+        with memfile.open(**output_profile) as mem:
+            mem.write(np.rot90(variable[:]), indexes=1)        
+        cog_translate(
+            memfile,
+            f"{os.path.splitext(src_path)[0]}.tif",
+            output_profile,
+            config=dict(GDAL_NUM_THREADS="ALL_CPUS", GDAL_TIFF_OVR_BLOCKSIZE="128"),
+        )
+
+to_cog(**f1)
