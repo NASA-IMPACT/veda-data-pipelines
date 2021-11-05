f902a1c3f8a8a3ab53862705a9f1c46b6abcc140
Aimee Barciauskas <aimee@developmentseed.org>
2021-11-05 17:41:34 -0600

    Ab/working on omi (#44)
    * Add readme and configuration
    
    * No more netrc
    
    * Remove netrc req
    
    * Update instructions
    
    * Update instructions
    
    * Fix headers
    
    * Working on NISAR HDF5
    
    * Add more scripts for nisar
    
    * Gitignore h5 and he5 files
    
    * Add indexes
    
    * Working on OMI NO2
    
    * update with OMI configuration
    
    * a few add'l python commands
    
    * Merge omi
    
    * Updates for GPM IMERG

---
+++ docker/hdf5-to-cog/ERA5/fetch.py
@@ -0,0 +1,20 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'format': 'netcdf',
+        'variable': 'cloud_base_height',
+        'year': '2021',
+        'month': '08',
+        'day': [
+            '01'
+        ],
+        'time': [
+            '00:00'
+        ],
+    },
+    'download.nc')
