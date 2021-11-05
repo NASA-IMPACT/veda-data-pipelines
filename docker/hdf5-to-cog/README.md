e019c0a12147a0766e2f46c66c8ef6ec7bdb8b17
Aimee Barciauskas <aimee@developmentseed.org>
2021-11-05 17:41:34 -0600

    Ab/working on omi (#44)
    * Add readme and configuration
    
    * No more netrc
    
    * Remove netrc req
    
    * Update instructions
    
    * Update instructions
    
    * Fix headers
    
    * Working on NISAR HDF5
    
    * Add more scripts for nisar
    
    * Gitignore h5 and he5 files
    
    * Add indexes
    
    * Working on OMI NO2
    
    * update with OMI configuration
    
    * a few add'l python commands
    
    * Merge omi
    
    * Updates for GPM IMERG

---
+++ docker/hdf5-to-cog/README.md
@@ -0,0 +1,53 @@
+# NetCDF4 / HDF5 to COG
+
+🚧 WIP 🚧 Configurable module for converting NetCDF4 / HDF5 to COG.
+
+At this time, just 2 configurations have been made in `example.ini`.
+
+Before running the commands below, make sure you `cd docker/hdf5-to-cog`.
+
+## GPM IMERG Example
+
+GPM IMERG data is fetched over HTTP using `.netrc` for URS credentials. Current configuration is for the preciptation variable.
+
+Add to your `~/.netrc` file
+
+```bash
+machine urs.earthdata.nasa.gov
+	login aimeeb
+	password XXX
+```
+
+Run the transofrm
+
+```bash
+# Test it
+export USERNAME=aimeeb
+export PASSWORD=XXX
+unset GDAL_DATA
+pyenv exec python run.py \
+  -c GPM_3IMERGM \
+  -f https://gpm1.gesdisc.eosdis.nasa.gov/data/GPM_L3/GPM_3IMERGM.06/2020/3B-MO.MS.MRG.3IMERG.20200501-S000000-E235959.05.V06B.HDF5
+```
+
+## ERA5 Cloud Base Height Example
+
+ERA5 data is fetched using `cdsapi` library which first requires registration and API configuration, see https://cds.climate.copernicus.eu/api-how-to for instructions. 
+
+Current configuration is for the cloud base height variable.
+
+```bash
+# First fetch the data
+python3 ERA5/fetch.py
+# Generate the cog
+python3 run.py -f download.nc -c ERA5
+```
+
+## OMI NO2 Example
+
+```bash
+# Test it
+python3 run.py \
+  -c OMINO2 \
+  -f ../../sample-files/OMI-Aura_L3-OMNO2d_2004m1001_v003-2019m1121t082956.he5
+```
--- docker/hdf5-to-cog/run.py
+++ docker/hdf5-to-cog/run.py
@@ -4,24 +4,22 @@ from rasterio.crs import CRS
 from rasterio.io import MemoryFile
 from rio_cogeo.cogeo import cog_translate
 from rio_cogeo.profiles import cog_profiles
+from rasterio.warp import calculate_default_transform
 import numpy as np
 import argparse
 import os
+import requests
 import boto3
+from typing import Optional
+import configparser
+config = configparser.ConfigParser()
+config.read('example.ini')
 
 parser = argparse.ArgumentParser(description="Generate COG from file and schema")
 parser.add_argument("-f", "--filename", help="HDF5 or NetCDF filename to convert")
+parser.add_argument('-c', '--collection', help='Collection config to use in conversion')
 args = parser.parse_args()
 s3 = boto3.client('s3')
-
-output_bucket = 'cumulus-map-internal'
-output_dir = 'cloud-optimized'
-
-# input file schema
-f1 = dict(
-    group="Grid",
-    variable_name="precipitationCal"
-)
 
 # Set COG inputs
 output_profile = cog_profiles.get(
@@ -29,57 +27,93 @@ output_profile = cog_profiles.get(
 )  # if the files aren't uint8, this will need to be changed
 output_profile["blockxsize"] = 256
 output_profile["blockysize"] = 256
-
-def rename(filename):
-    """
-    This is specific to GPM IMERG product
-    """
-    imerg_date = filename.split(".")[4].split('-')[0]
-    replacement_date = f"{imerg_date[0:4]}_{imerg_date[4:6]}_{imerg_date[6:8]}"
-    return f"{os.path.splitext(filename.replace(imerg_date, replacement_date))[0]}.tif"
+output_bucket = config['DEFAULT']['output_bucket']
+output_dir = config['DEFAULT']['output_dir']
 
 def upload_file(outfilename, collection):
     return s3.upload_file(
-        outfilename, output_bucket, f"{output_dir}/{collection}/{outfilename}"
+        outfilename, output_bucket, f"{output_dir}/{collection}/{outfilename}",
+        ExtraArgs={'ACL': 'public-read'}
     )
 
-def download_file(s3_path: str):
-    filename = os.path.basename(s3_path)
-    path_parts = s3_path.split('://')[1].split('/')
-    bucket = path_parts[0]
-    path = '/'.join(path_parts[1:])
-    collection = path_parts[-2]
-    s3.download_file(bucket, path, filename)
-    return dict(filename=filename, collection=collection)
+def download_file(file_uri: str):
+    filename = os.path.basename(file_uri)
+    print(filename)
+    if 'http' in file_uri:
+        # This isn't working for GPMIMERG, need to use .netrc
+        username = os.environ.get('USERNAME')
+        password = os.environ.get('PASSWORD')
+        session = requests.Session()
+        session.auth = (username, password)
+        response = session.get(file_uri)
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+    elif 's3://' in file_uri:
+        path_parts = file_uri.split('://')[1].split('/')
+        bucket = path_parts[0]
+        path = '/'.join(path_parts[1:])
+        s3.download_file(bucket, path, filename) 
+    else:
+        print(f"{filename} file already downloaded")
+    return filename
 
-def to_cog(
-        src_filename: str,
-        group: str,
-        variable_name: str):
+def to_cog(**config):
     """HDF5 to COG."""
     # Open existing dataset
-    src = Dataset(src_filename, "r")
-    variable = src.groups[group][variable_name][:]
-    xmin, ymin, xmax, ymax = [-180, -90, 180, 90]
-    nrows, ncols = variable.shape[0], variable.shape[1]
-    print("nrows, ncols: ", nrows, ncols)
-    # TODO: Review - flipping IMERG
-    xres = (xmax - xmin) / float(nrows)
-    yres = (ymax - ymin) / float(ncols)
-    geotransform = (xmin, xres, 0, ymax, 0, -yres)
-    dst_transform = Affine.from_gdal(*geotransform)
-    nodata_value = variable.fill_value
+    filename = config['filename']
+    variable_name = config['variable_name']
+    x_variable, y_variable = config.get('x_variable'), config.get('y_variable')
+    group = config.get('group')
+    src = Dataset(filename, "r")
+    if group is None:
+        variable = src[variable_name][:]
+        nodata_value = variable.fill_value
+    else:
+        variable = src.groups[group][variable_name]
+        nodata_value = variable._FillValue
+    # This may be just what we need for IMERG
+    if collection == 'GPM_3IMERGM':
+        variable = np.transpose(variable[0])
+    # This implies a global spatial extent, which is not always the case
+    src_height, src_width = variable.shape[0], variable.shape[1]    
+    if x_variable and y_variable:
+        xmin = src[x_variable][:].min()
+        xmax = src[x_variable][:].max()
+        ymin = src[y_variable][:].min()
+        ymax = src[y_variable][:].max()
+    else:
+        xmin, ymin, xmax, ymax = [-180, -90, 180, 90]
 
+    src_crs = config.get('src_crs')
+    if src_crs:
+        src_crs = CRS.from_proj4(src_crs)
+    else:
+        src_crs = CRS.from_epsg(4326)
+
+    dst_crs = CRS.from_epsg(3857)
+
+    # calculate dst transform
+    dst_transform, dst_width, dst_height = calculate_default_transform(
+        src_crs, dst_crs, src_width, src_height, left=xmin, bottom=ymin, right=xmax, top=ymax
+    )
+
+    # https://github.com/NASA-IMPACT/cloud-optimized-data-pipelines/blob/rwegener2-envi-to-cog/docker/omno2-to-cog/OMNO2d.003/handler.py
+    affine_transformation = config.get('affine_transformation')
+    if affine_transformation:
+        xres = (xmax - xmin) / float(src_width)
+        yres = (ymax - ymin) / float(src_height)
+        geotransform = eval(affine_transformation)
+        dst_transform = Affine.from_gdal(*geotransform)
+    
     # Save output as COG
     output_profile = dict(
         driver="GTiff",
         dtype=variable.dtype,
         count=1,
-        # TODO: Review - flipping IMERG
-        height=ncols,
-        width=nrows,
-        crs=CRS.from_epsg(4326),
+        crs=src_crs,
         transform=dst_transform,
+        height=dst_height,
+        width=dst_width,
         nodata=nodata_value,
         tiled=True,
         compress="deflate",
@@ -87,28 +121,28 @@ def to_cog(
         blockysize=256,
     )
     print("profile h/w: ", output_profile["height"], output_profile["width"])
+    outfilename = f'{filename}.tif'
     with MemoryFile() as memfile:
         with memfile.open(**output_profile) as mem:
-            # TODO: Review - flipping IMERG
-            mem.write(np.rot90(variable[:]), indexes=1) 
-        outfilename = rename(src_filename)
+            data = variable.astype(np.float32)
+            mem.write(data, indexes=1)
         cog_translate(
             memfile,
-            rename(src_filename),
+            outfilename,
             output_profile,
             config=dict(GDAL_NUM_THREADS="ALL_CPUS", GDAL_TIFF_OVR_BLOCKSIZE="128"),
         )
-        return outfilename
+    return outfilename
 
-if os.environ.get('ENV') != 'test':
-    s3_path = args.filename
-    file_args = download_file(s3_path=s3_path)
-    collection, filename = file_args['collection'], file_args['filename']
+filename = args.filename
+collection = args.collection
+to_cog_config = config._sections[collection]
+
+if os.environ.get('DOWNLOAD') == 'true':
+    downloaded_filename = download_file(file_uri=filename)
+    to_cog_config['filename'] = downloaded_filename
 else:
-    filename = args.filename
+    to_cog_config['filename'] = filename
 
-f1['src_filename'] = filename
-outfilename = to_cog(**f1)
-
-if os.environ.get('ENV') != 'test':
-    upload_file(outfilename, collection)
+outfilename = to_cog(**to_cog_config)
+print(outfilename)
