977ed880ae53133a7e05794563d8b2dec40e14aa
Aimee Barciauskas <aimee@developmentseed.org>
2022-01-07 12:33:36 -0500

    Working on CMR query as lambda function

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,12 +1,37 @@
 from aws_cdk import core
 import aws_cdk.aws_stepfunctions as stepfunctions
+import aws_cdk.aws_events as events
+import aws_cdk.aws_events_targets as targets
+from aws_cdk import aws_lambda
 
 class CdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        ## State Machine and Rule to run it
         start_state = stepfunctions.Pass(self, "StartState")
         simple_state_machine = stepfunctions.StateMachine(self, "SkeletonStateMachine",
             definition=start_state
         )
-        # The code that defines your stack goes here
+        rule = events.Rule(self, "Schedule Rule",
+            schedule=events.Schedule.cron(minute="1")
+        )
+        rule.add_target(targets.SfnStateMachine(simple_state_machine))
+
+        # Discover function
+        code_dir = "./"
+        discover_function = aws_lambda.Function(
+            self,
+            f"modis-aod-discover-function",
+            code=aws_lambda.Code.from_asset_image(
+                directory=code_dir,
+                file="cmr-query/Dockerfile",
+                entrypoint=["/usr/local/bin/python", "-m", "awslambdaric", "-c", "OMNO2d", "-d", 7, "--include ", "^.+he5$"],
+                cmd=["handler.handler"],
+            ),
+            handler=aws_lambda.Handler.FROM_IMAGE,
+            runtime=aws_lambda.Runtime.FROM_IMAGE,
+            memory_size=1024,
+            timeout=core.Duration.seconds(30)
+        )
--- cmr-query/run.py
+++ cmr-query/run.py
@@ -4,32 +4,37 @@ import json
 from cmr import GranuleQuery
 import re
 
+def handler(event, context):
+    """
+    Lambda handler for the NetCDF ingestion pipeline
+    """
+    collection = event.collection
+
+    enddate = dt.datetime.now()
+    startdate = enddate - dt.timedelta(days=event.days)
+    print(f"Querying for {collection} granules from {startdate} to {enddate}")
+
+    api = GranuleQuery()
+    granules = api.short_name(collection).temporal(startdate, enddate).get(5)
+
+    granule = granules[0]
+    urls = []
+    for link in granule['links']:
+        if link['rel'] == 'http://esipfed.org/ns/fedsearch/1.1/data#':
+            href = link['href']
+            if event.include:
+                pattern = re.compile(event.include)
+                matched = pattern.match(href)
+                if matched:
+                    urls.append(href)
+            else:
+                urls.append(href)
+
+    return urls
+
 parser = argparse.ArgumentParser(description="Query CMR for recent files from a given collection")
-parser.add_argument("-d", "--days", help="How many days past to query", default=7)
+parser.add_argument("-d", "--days", help="How many days past to query", type=int, default=7)
 parser.add_argument('-c', '--collection', help='Collection short name', default="OMNO2d")
 parser.add_argument('--include', help='only inlude links which match this regex')
-args = parser.parse_args()
-collection = args.collection
-
-enddate = dt.datetime.now()
-startdate = enddate - dt.timedelta(days=args.days)
-print(f"Querying for {collection} granules from {startdate} to {enddate}")
-
-api = GranuleQuery()
-granules = api.short_name(collection).temporal(startdate, enddate).get(5)
-
-granule = granules[0]
-urls = []
-for link in granule['links']:
-    if link['rel'] == 'http://esipfed.org/ns/fedsearch/1.1/data#':
-        href = link['href']
-        if args.include:
-            pattern = re.compile(args.include)
-            matched = pattern.match(href)
-            if matched:
-                urls.append(href)
-        else:
-            urls.append(href)
-
-print(urls)
-c
+event = parser.parse_args()
+print(handler(event, {}))
