ad0f9199b683ce57efd0dac3aa4d446ff1030fb8
Jeevan Farias <jeevan@developmentseed.org>
2022-01-13 15:30:24 -0500

    Add granule id to cmr-query file object return

--- cmr-query/handler.py
+++ cmr-query/handler.py
@@ -1,32 +1,36 @@
 import datetime as dt
 from cmr import GranuleQuery
 import re
+import json
+
 
 def handler(event, context):
     """
     Lambda handler for the NetCDF ingestion pipeline
     """
-    collection = event['collection']
-    version = event['version']
+    collection = event["collection"]
+    version = event["version"]
 
     enddate = dt.datetime.now()
-    startdate = enddate - dt.timedelta(hours=event['hours'])
+    startdate = enddate - dt.timedelta(hours=event["hours"])
     print(f"Querying for {collection} granules from {startdate} to {enddate}")
 
     api = GranuleQuery()
-    granules = api.short_name(collection).version(version).temporal(startdate, enddate).get_all()
+    granules = (
+        api.short_name(collection)
+        .version(version)
+        .temporal(startdate, enddate)
+        .get_all()
+    )
 
     urls = []
     for granule in granules:
-        for link in granule['links']:
-            if link['rel'] == 'http://esipfed.org/ns/fedsearch/1.1/data#':
-                href = link['href']
-                file_obj = {
-                    "collection": collection,
-                    "href": href
-                }
-                if event['include']:
-                    pattern = re.compile(event['include'])
+        for link in granule["links"]:
+            if link["rel"] == "http://esipfed.org/ns/fedsearch/1.1/data#":
+                href = link["href"]
+                file_obj = {"collection": collection, "href": href, "granule_id": granule['id']}
+                if event["include"]:
+                    pattern = re.compile(event["include"])
                     matched = pattern.match(href)
                     if matched:
                         urls.append(file_obj)
@@ -35,11 +39,12 @@ def handler(event, context):
     print(f"Returning urls {urls}")
     return urls
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sample_event = {
         "hours": 48,
         "collection": "OMNO2d",
         "version": "003",
-        "include": "^.+he5$"
-    }    
+        "include": "^.+he5$",
+    }
     handler(sample_event, {})
