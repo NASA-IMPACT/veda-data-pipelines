d1ead4d27d2d024537289100b075ca3234728fba
Jeevan Farias <jeevan@developmentseed.org>
2022-01-20 12:44:14 -0500

    Do everything with rio stac

--- stac-gen/handler.py
+++ stac-gen/handler.py
@@ -3,7 +3,7 @@ import json
 import pystac
 from pystac.utils import str_to_datetime
 from shapely.geometry import shape
-from rio_stac.stac import bbox_to_geom
+from rio_stac.stac import bbox_to_geom, create_stac_item
 
 
 def try_int(x):
@@ -13,38 +13,34 @@ def try_int(x):
         return x
 
 
-def create_item(entry):
-    bbox = [try_int(x) for x in entry["boxes"][0].split(" ")]
+def create_item(cmr, cog, collection):
 
-    dt = str_to_datetime(entry["updated"])
+    assets = {}
 
-    geometry = bbox_to_geom(bbox)
-
-    granule_id = entry["id"]
-
-    props = {
-        "coordinate_system": entry["coordinate_system"],
-        "day_night_flag": entry["day_night_flag"],
-    }
-
-    item = pystac.Item(
-        id=granule_id, geometry=geometry, bbox=bbox, datetime=dt, properties=props
-    )
-
-    for link in entry["links"]:
+    for link in cmr["links"]:
         if ".he5" in link["href"]:
             name = link["title"] if "title" in link else link["href"]
-            item.add_asset(
-                name,
-                pystac.Asset(
+            assets[name] = pystac.Asset(
                     href=link["href"],
                     media_type="application/x-hdf5",
                     roles=["data"],
                     title="hdf image",
-                ),
-            )
+                )
 
-    return item
+    dt = str_to_datetime(cmr["updated"])
+
+
+    rstac = create_stac_item(
+        source=cog,
+        collection=collection,
+        input_datetime=dt,
+        properties=cmr,
+        with_proj=True,
+        with_raster=True,
+        assets=assets
+    )
+
+    return rstac
 
 
 def handler(event, context):
@@ -59,7 +55,10 @@ def handler(event, context):
     concept_id = event["granule_id"]
     cmr_json = api.concept_id(concept_id).get(1)
 
-    stac_item = create_item(cmr_json[0])
+    cog = event['s3_filename']
+    collection = event['collection']
+
+    stac_item = create_item(cmr=cmr_json[0], cog=cog, collection=collection)
     print("Created item...")
 
 
@@ -67,6 +66,7 @@ if __name__ == "__main__":
     sample_event = {
         "collection": "OMNO2d",
         "href": "https://acdisc.gesdisc.eosdis.nasa.gov/data//Aura_OMI_Level3/OMNO2d.003/2022/OMI-Aura_L3-OMNO2d_2022m0111_v003-2022m0112t181633.he5",
+        "s3_filename": "s3://climatedashboard-data/OMDOAO3e/OMI-Aura_L3-OMDOAO3e_2022m0105_v003-2022m0107t023328.he5.tif",
         "granule_id": "G2199243759-GES_DISC",
     }
 
