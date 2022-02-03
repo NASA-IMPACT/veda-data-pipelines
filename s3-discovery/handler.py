0ec08d770dddf0733c47e77d174565acd49e8e53
Jeevan Farias <jeevan@developmentseed.org>
2022-02-03 16:09:34 -0500

    Add s3 discovery dir

---
+++ s3-discovery/handler.py
@@ -0,0 +1,57 @@
+import boto3
+
+s3 = boto3.resource(
+    "s3",
+)
+
+
+def list_bucket(bucket, prefix, file_type):
+    try:
+        files = []
+        bucket = s3.Bucket(bucket)
+        for obj in bucket.objects.filter(Prefix=prefix):
+            if file_type:
+                if obj.key.endswith(file_type):
+                    files.append(obj.key)
+            else:
+                files.append(obj.key)
+        return files
+
+    except Exception as e:
+        print(e)
+    return e
+
+
+def handler(event, context):
+    filenames = list_bucket(
+        bucket=event["bucket"], prefix=event["prefix"], file_type=event["file_type"]
+    )
+
+    files_objs = []
+    for f in filenames:
+        files_objs.append(
+            {
+                # Remove trailing back slash used for prefixing
+                "collection": event["prefix"][:-1],
+                "s3_filename": f's3://{event["bucket"]}/{f}',
+                "datetime_regex": {
+                    "regex": "^(.*?)(_)([0-9][0-9][0-9][0-9])(.tif)$",
+                    "target_group": 3,
+                },
+            }
+        )
+    print(files_objs)
+
+    return files_objs
+
+
+if __name__ == "__main__":
+    sample_event = {
+        "bucket": "climatedashboard-data",
+        # Directory
+        "prefix": "OMSO2PCA/",
+        # File type
+        "file_type": ".tif",
+    }
+
+    handler(sample_event, {})
