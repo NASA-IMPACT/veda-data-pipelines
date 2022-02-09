035ab95e265394078b0c141cbb42a7b064c2ce51
Jeevan Farias <jeevan@developmentseed.org>
2022-02-09 09:29:15 -0500

    Move dt regex up one level

--- s3-discovery/handler.py
+++ s3-discovery/handler.py
@@ -34,10 +34,7 @@ def handler(event, context):
                 # Remove trailing back slash used for prefixing
                 "collection": event["prefix"][:-1],
                 "s3_filename": f's3://{event["bucket"]}/{f}',
-                "datetime_regex": {
-                    "regex": f"^(.*?)(_)([0-9][0-9][0-9][0-9])({event['file_type'})$",
-                    "target_group": 3,
-                },
+                "datetime_regex": event["datetime_regex"],
             }
         )
     print(files_objs)
@@ -52,6 +49,10 @@ if __name__ == "__main__":
         "prefix": "OMSO2PCA/",
         # File type
         "file_type": ".tif",
+        "datetime_regex": {
+            "regex": f"^(.*?)(_)([0-9][0-9][0-9][0-9])(.tif)$",
+            "target_group": 3
+        }
     }
 
     handler(sample_event, {})
