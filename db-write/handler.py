5d9a9f04b9a2b7763ebdcee3f514d275427325e4
Jeevan Farias <jeevan@developmentseed.org>
2022-02-16 14:38:35 -0500

    Fix typo in db write

--- db-write/handler.py
+++ db-write/handler.py
@@ -13,6 +13,7 @@ STAC_DB_HOST = os.environ.get("STAC_DB_HOST")
 STAC_DB_USER = os.environ.get("STAC_DB_USER")
 STAC_DB_PASSWORD = os.environ.get("STAC_DB_PASSWORD")
 
+
 def handler(event, context):
     """
     Simple Lambda that should belong to the VPC of the STAC_DB_HOST
@@ -36,14 +37,89 @@ def handler(event, context):
         return e
     os.remove("/tmp/temp.json")
 
-    return stac_dict
-
+    return stac_json
 
 if __name__ == "__main__":
-    sample_event = {
-        "stac_item": {
-            "id": 1
-        }
-    },
+    sample_event = (
+        {
+            "stac_item": {
+                "assets": {
+                    "cog": {
+                        "href": "s3://climatedashboard-data/BMHD_Ida/BMHD_Ida2021_NO_LA_August9.tif",
+                        "roles": ["data"],
+                        "title": "COG",
+                        "type": "image/tiff; application=geotiff",
+                    }
+                },
+                "bbox": [
+                    -90.3037818244749,
+                    29.804659612978707,
+                    -89.87578181971654,
+                    30.07177072705947,
+                ],
+                "collection": "BMHD_Ida",
+                "geometry": {
+                    "coordinates": [
+                        [
+                            [-90.3037818244749, 30.07177072705947],
+                            [-90.3037818244749, 29.804659612978707],
+                            [-89.87578181971654, 29.804659612978707],
+                            [-89.87578181971654, 30.07177072705947],
+                            [-90.3037818244749, 30.07177072705947],
+                        ]
+                    ],
+                    "type": "Polygon",
+                },
+                "id": "BMHD_Ida2021_NO_LA_August9.tif",
+                "links": [
+                    {
+                        "href": "BMHD_Ida",
+                        "rel": "collection",
+                        "type": "application/json",
+                    }
+                ],
+                "properties": {
+                    "datetime": "2021-08-09T00:00:00Z",
+                    "proj:bbox": [
+                        -90.3037818244749,
+                        29.804659612978707,
+                        -89.87578181971654,
+                        30.07177072705947,
+                    ],
+                    "proj:epsg": 4326,
+                    "proj:geometry": {
+                        "coordinates": [
+                            [
+                                [-90.3037818244749, 30.07177072705947],
+                                [-90.3037818244749, 29.804659612978707],
+                                [-89.87578181971654, 29.804659612978707],
+                                [-89.87578181971654, 30.07177072705947],
+                                [-90.3037818244749, 30.07177072705947],
+                            ]
+                        ],
+                        "type": "Polygon",
+                    },
+                    "proj:shape": [2404, 3852],
+                    "proj:transform": [
+                        0.00011111111234640703,
+                        0.0,
+                        -90.3037818244749,
+                        0.0,
+                        -0.00011111111234640703,
+                        30.07177072705947,
+                        0.0,
+                        0.0,
+                        1.0,
+                    ],
+                },
+                "stac_extensions": [
+                    "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
+                    "https://stac-extensions.github.io/raster/v1.1.0/schema.json",
+                ],
+                "stac_version": "1.0.0",
+                "type": "Feature",
+            }
+        },
+    )
 
     handler(sample_event, {})
--- stac-gen/handler.py
+++ stac-gen/handler.py
@@ -168,25 +168,11 @@ def handler(event, context):
     except Exception as e:
         print(e)
         return
+    print(json.dumps(stac_dict, indent=4, sort_keys=True))
 
-    # pypgstac requires inserting from a file
-    with open("/tmp/temp.json", "w+") as f:
-        f.write(json.dumps(stac_dict))
-
-    try:
-        pypgstac.load(
-            table="items",
-            file="/tmp/temp.json",
-            dsn=f"postgres://{STAC_DB_USER}:{STAC_DB_PASSWORD}@{STAC_DB_HOST}/postgis",
-            method="insert_ignore",  # use insert_ignore to avoid overwritting existing items
-        )
-        print("Inserted to database")
-    except Exception as e:
-        print(e)
-        return e
-    os.remove("/tmp/temp.json")
-
-    return stac_dict
+    return {
+        "stac_item": stac_dict
+    }
 
 
 if __name__ == "__main__":
