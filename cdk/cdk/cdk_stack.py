1d3c7b41dabbdc9dbcdbd16917cbc1ee23e51048
Jeevan Farias <jeevan@developmentseed.org>
2022-02-18 14:35:01 -0500

    Add sqs, sendMessage after writing json to s3

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,10 +1,12 @@
-from aws_cdk import core, aws_iam
+from aws_cdk import core, aws_iam, custom_resources
 import aws_cdk.aws_stepfunctions as stepfunctions
 import aws_cdk.aws_events as events
 import aws_cdk.aws_events_targets as targets
 from aws_cdk import aws_lambda
 from aws_cdk import aws_stepfunctions_tasks as tasks
 from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_sqs as sqs
+from aws_cdk import aws_s3
 import os
 
 VPC_ID = "vpc-0b0926e1be04a588d"
@@ -19,6 +21,10 @@ class CdkStack(core.Stack):
         bucket = "climatedashboard-data"
         prefix = "OMSO2PCA/"
 
+        s3bucket = aws_s3.Bucket.from_bucket_name(
+            self, f"{id}-bucket", bucket_name=bucket
+        )
+
         ec2_network_access = aws_iam.PolicyStatement(
             actions=[
                 "ec2:CreateNetworkInterface",
@@ -28,11 +34,83 @@ class CdkStack(core.Stack):
             resources=["*"],
         )
         full_bucket_access = aws_iam.PolicyStatement(
-            actions=["s3:GetObject"],
+            actions=["s3:GetObject", "s3:PutObject"],
             resources=[f"arn:aws:s3:::{bucket}/*"],
         )
 
         database_vpc = ec2.Vpc.from_lookup(self, f"{id}-vpc", vpc_id=VPC_ID)
+
+        ingest_queue = sqs.Queue(
+            self,
+            f"{id}-ingest-queue",
+            dead_letter_queue=sqs.DeadLetterQueue(
+                max_receive_count=5,
+                queue=sqs.Queue(self, id=f"{id}-cog-ingest-dead-letter-queue"),
+            ),
+            # same visibility as lambda function
+            visibility_timeout=core.Duration.seconds(30),
+        )
+        ingest_queue.add_to_resource_policy(
+            aws_iam.PolicyStatement(
+                principals=[aws_iam.ServicePrincipal("s3.amazonaws.com")],
+                actions=["SQS:SendMessage"],
+                resources=[ingest_queue.queue_arn],
+                conditions={"ArnEquals": {"aws:SourceArn": s3bucket.bucket_arn}},
+            )
+        )
+
+        # In CDK, imported buckets implement IBucketProxy, as opposed to
+        # Bucket, which makes it impossible to add an event notification
+        # to an imported bucket using the `bucket.add_event_notification()`
+        # function. Instead we have to implement the bucket notification
+        # as a custome S3 SDK call
+        custom_s3_sdk_call = custom_resources.AwsSdkCall(
+            service="S3",
+            action="putBucketNotificationConfiguration",
+            parameters={
+                "Bucket": s3bucket.bucket_name,
+                "NotificationConfiguration": {
+                    "QueueConfigurations": [
+                        {
+                            "Events": ["s3:ObjectCreated:*"],
+                            "Filter": {
+                                "Key": {
+                                    "FilterRules": [
+                                        {"Name": "prefix", "Value": "stac_item_queue"},
+                                        {"Name": "suffix", "Value": ".json"},
+                                    ]
+                                }
+                            },
+                            "QueueArn": ingest_queue.queue_arn,
+                        }
+                    ]
+                },
+            },
+            physical_resource_id=custom_resources.PhysicalResourceId.of(
+                f"s3-notification-resource-cog-pipelines-1"
+            ),
+        )
+
+        custom_notification = custom_resources.AwsCustomResource(
+            self,
+            "s3-sqs-notification",
+            policy=custom_resources.AwsCustomResourcePolicy.from_statements(
+                [
+                    aws_iam.PolicyStatement(
+                        effect=aws_iam.Effect.ALLOW,
+                        resources=[s3bucket.bucket_arn],
+                        actions=["s3:PutBucketNotification"],
+                    )
+                ]
+            ),
+            on_create=custom_s3_sdk_call,
+            on_update=custom_s3_sdk_call,
+            # TODO: does `on_delete` needs to be implemented as well?
+            # See: https://stackoverflow.com/a/61641858/15462881
+        )
+
+        custom_notification.node.add_dependency(s3bucket)
+        custom_notification.node.add_dependency(ingest_queue)
 
         lambda_function_security_group = ec2.SecurityGroup(
             self,
@@ -210,7 +288,9 @@ class CdkStack(core.Stack):
         )
 
         # Generate a cog and create stac item for each element
-        map_cogs.iterator(generate_cog_task.next(cmr_generate_stac_item_task).next(cmr_db_write_task))
+        map_cogs.iterator(
+            generate_cog_task.next(cmr_generate_stac_item_task).next(cmr_db_write_task)
+        )
 
         map_stac_items = stepfunctions.Map(
             self,
