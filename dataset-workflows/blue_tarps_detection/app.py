006592770178b72790c0aa4bce8bfa5331540468
xhagrg <grg.iksha@hotmail.com>
2022-03-28 09:08:10 -0500

    Add workflows for blue-tarp detection and planetscope segmented files.

---
+++ dataset-workflows/blue_tarps_detection/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+from aws_cdk import core
+
+from cdk.cdk_stack import CdkStack
+
+
+app = core.App()
+CdkStack(
+    app,
+    "cog-pipeline-blue_tarp_deteciton-staging",
+    env=dict(
+        region=os.environ["CDK_DEFAULT_REGION"],
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+    ),
+)
+
+app.synth()
---
+++ dataset-workflows/blue_tarps_planetscope/config.py
@@ -0,0 +1,5 @@
+import os
+
+
+# VPC that contains the target database to which STAC records will be inserted
+VPC_ID = os.environ.get("VPC_ID")
