4d0947cc959fc1cf6be7098c2cee3688d84e293f
xhagrg <grg.iksha@hotmail.com>
2022-03-30 15:33:37 -0500

    Use generic collection for black marbel data.

--- scripts/collections-sql/black_marble_hd.sql
+++ scripts/collections-sql/black_marble_hd.sql
@@ -1,9 +1,9 @@
 INSERT INTO pgstac.collections (content) VALUES('{
-    "id": "BMHD_Ida",
+    "id": "BMHD",
    "type": "Collection",
    "links":[
    ],
-   "title":"BMHD_Ida",
+   "title":"BMHD",
    "extent":{
       "spatial":{
          "bbox":[
@@ -18,7 +18,7 @@ INSERT INTO pgstac.collections (content) VALUES('{
       "temporal":{
          "interval":[
             [
-               "2021-08-09T00:00:00Z",
+               "2017-07-21T00:00:00Z",
                "2021-08-31T00:00:00Z"
             ]
          ]
