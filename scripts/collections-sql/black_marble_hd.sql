2d1582523bae5a874de453f6b60766b80b2110bb
xhagrg <grg.iksha@hotmail.com>
2022-04-04 15:02:10 -0500

    Update sql to reflect proper values for nightlight-hd-monthly collection.

--- scripts/collections-sql/black_marble_hd.sql
+++ scripts/collections-sql/black_marble_hd.sql
@@ -1,9 +1,9 @@
 INSERT INTO pgstac.collections (content) VALUES('{
-    "id": "BMHD",
+   "id": "nightlights-hd-monthly",
    "type": "Collection",
    "links":[
    ],
-   "title":"BMHD",
+   "title":"Black Marble High Definition Nightlights Monthly Dataset",
    "extent":{
       "spatial":{
          "bbox":[
@@ -19,14 +19,27 @@ INSERT INTO pgstac.collections (content) VALUES('{
          "interval":[
             [
                "2017-07-21T00:00:00Z",
-               "2021-08-31T00:00:00Z"
+               "2021-09-30T23:59:59Z"
             ]
          ]
       }
    },
    "license":"public-domain",
-   "description":"BMHDIda",
-   "stac_version":"1.0.0"
+   "description": "The High Definition Nightlights dataset is processed to eliminate light sources, including moonlight reflectance and other interferences. Darker colors indicate fewer night lights and less activity. Lighter colors indicate more night lights and more activity.",
+   "stac_version": "1.0.0",
+   "dashboard:is_periodic": false,
+   "dashboard:time_density": "month"
+   "item_assets": {
+      "cog_default": {
+         "type": "image/tiff; application=geotiff; profile=cloud-optimized",
+         "roles": [
+            "data",
+            "layer"
+         ],
+         "title": "Default COG Layer",
+         "description": "Cloud optimized default layer to display on map"
+      }
+   }
 }')
 ON CONFLICT (id) DO UPDATE 
   SET content = excluded.content;
