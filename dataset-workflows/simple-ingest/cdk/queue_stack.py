2e03825bb62db4247106a370c9abbef6f69daeb4
“slesaad” <slesaad@gmail.com>
2022-05-06 19:52:30 -0500

    Update queues and permissions

--- dataset-workflows/simple-ingest/cdk/queue_stack.py
+++ dataset-workflows/simple-ingest/cdk/queue_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
     core,
     aws_sqs as sqs,
-    aws_s3 as s3,
     aws_lambda_event_sources as lambda_event_sources,
 )
 
@@ -10,52 +9,50 @@ class QueueStack(core.Stack):
     def __init__(self, app, construct_id, lambda_stack, **kwargs) -> None:
         super().__init__(app, construct_id, **kwargs)
 
-        build_ndjson_lambda = lambda_stack.lambdas["build_ndjson_lambda"]
-        pgstac_loader_lambda = lambda_stack.lambdas["pgstac_loader_lambda"]
-
-        ndjson_bucket = self._bucket(f"cogpipeline-ndjson")
-        ndjson_bucket.grant_read_write(build_ndjson_lambda.role)
-        ndjson_bucket.grant_read(pgstac_loader_lambda.role)
+        trigger_cogify_lambda = lambda_stack.lambdas["trigger_cogify_lambda"]
+        trigger_ingest_lambda = lambda_stack.lambdas["trigger_ingest_lambda"]
+        cogify_or_not_lambda = lambda_stack.lambdas["cogify_or_not_lambda"]
+        cogify_lambda = lambda_stack.lambdas["cogify_lambda"]
         
-        discovered_queue = self._queue(f"{construct_id}-discovered-queue",
+        cogify_queue = self._queue(f"{construct_id}-cogify-queue",
             visibility_timeout=600,
             dead_letter_queue=sqs.DeadLetterQueue(
                 max_receive_count=5,
-                queue=self._queue(f"{construct_id}-discovered-dlq"),
+                queue=self._queue(f"{construct_id}-cogify-dlq"),
             ),
         )
-        discovered_queue.grant_send_messages(lambda_stack.lambdas["cmr_discovery_lambda"].role)
+        cogify_queue.grant_send_messages(cogify_or_not_lambda.role)
 
-        build_ndjson_lambda.add_event_source(
+        trigger_cogify_lambda.add_event_source(
             lambda_event_sources.SqsEventSource(
-                discovered_queue,
+                cogify_queue,
                 batch_size=100,
-                max_batching_window=core.Duration.seconds(300),
+                max_batching_window=core.Duration.seconds(20),
                 report_batch_item_failures=True,
             )
         )
 
-        ndjson_queue = self._queue(f"{construct_id}-ndjson-queue",
+        stac_ready_queue = self._queue(f"{construct_id}-stac-ready-queue",
             visibility_timeout=600,
             dead_letter_queue=sqs.DeadLetterQueue(
                 max_receive_count=3,
-                queue=self._queue(f"{construct_id}-ndjson-dlq", retention_days=14),
+                queue=self._queue(f"{construct_id}-stac-ready-dlq", retention_days=14),
             )
         )
-        ndjson_queue.grant_send_messages(build_ndjson_lambda.role)
+        stac_ready_queue.grant_send_messages(cogify_or_not_lambda.role)
+        stac_ready_queue.grant_send_messages(cogify_lambda.role)
 
-        build_ndjson_lambda.add_environment("BUCKET", ndjson_bucket.bucket_name)
-        build_ndjson_lambda.add_environment("QUEUE_URL", ndjson_queue.queue_url)
+        cogify_or_not_lambda.add_environment("COGIFY_QUEUE_URL", cogify_queue.queue_url)
+        cogify_or_not_lambda.add_environment("STAC_READY_URL", stac_ready_queue.queue_url)
 
-        pgstac_loader_lambda.add_event_source(
+        trigger_ingest_lambda.add_event_source(
             lambda_event_sources.SqsEventSource(
-                ndjson_queue,
+                stac_ready_queue,
                 batch_size=100,
                 max_batching_window=core.Duration.seconds(300),
                 report_batch_item_failures=True,
             )
         )
-        ndjson_queue.grant_consume_messages(lambda_stack.lambdas["pgstac_loader_lambda"].role)
 
     def _queue(self, name, visibility_timeout=30, dead_letter_queue=None, retention_days=4):
         return sqs.Queue(
@@ -66,18 +63,3 @@ class QueueStack(core.Stack):
             dead_letter_queue=dead_letter_queue,
             retention_period=core.Duration.days(retention_days),
         )
-
-    def _bucket(self, name):
-        try:
-            ndjson_bucket = s3.Bucket(
-                self,
-                name,
-                bucket_name=name,
-            )
-        except:
-            ndjson_bucket = s3.Bucket.from_bucket_name(
-                self,
-                name,
-                bucket_name=name,
-            )
-        return ndjson_bucket
