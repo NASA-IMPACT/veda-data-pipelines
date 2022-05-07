7ceaf58520f2099f3b0200ddfa922677401914f0
“slesaad” <slesaad@gmail.com>
2022-05-06 19:43:18 -0500

    Update step function stack and lambda integration

--- dataset-workflows/simple-ingest/app.py
+++ dataset-workflows/simple-ingest/app.py
@@ -5,13 +5,20 @@ from aws_cdk import (
 )
 
 from cdk.lambda_stack import LambdaStack
-from cdk.iam_policies import IamPolicies
 from cdk.step_function_stack import StepFunctionStack
 from cdk.vpc_stack import VpcStack
 from cdk.queue_stack import QueueStack
+from cdk.iam_policies import IamPolicies
 
 import config
 
+def integrate_to_lambdas(trigger_cogify_lambda, trigger_ingest_lambda, cogify_arn, pub_arn):
+    # this lambda is triggered by SQS queue and in turn triggers execution of the publication step function
+    trigger_cogify_lambda.add_to_role_policy(IamPolicies.stepfunction_start_execution_access(cogify_arn))
+    trigger_ingest_lambda.add_to_role_policy(IamPolicies.stepfunction_start_execution_access(pub_arn))
+
+    trigger_cogify_lambda.add_environment("STEP_FUNCTION_ARN", cogify_arn)
+    trigger_ingest_lambda.add_environment("STEP_FUNCTION_ARN", pub_arn)
 
 app = core.App()
 
@@ -20,39 +27,44 @@ env_details = core.Environment(
     account=os.environ["CDK_DEFAULT_ACCOUNT"],
 )
 
-iam_stack = IamPolicies(
-    config.BUCKET_NAME
-)
-
 vpc_stack = VpcStack(
     app,
-    f"{config.APP_NAME}-vpc",
+    f"{config.APP_NAME}-{config.ENV}-vpc",
     vpc=config.VPC_ID,
     sg=config.SECURITY_GROUP_ID,
     env=env_details,
 )
 lambda_stack = LambdaStack(
     app,
-    f"{config.APP_NAME}-lambda",
+    f"{config.APP_NAME}-{config.ENV}-lambda",
     database_vpc=vpc_stack._database_vpc,
-    iam_stack=iam_stack,
     env=env_details
 )
 
 vpc_stack.add_rds_write_ingress(lambda_stack.lambda_sg)
 
-queue_stack = QueueStack(
+step_function_stack = StepFunctionStack(
     app,
-    f"{config.APP_NAME}-queue",
+    f"{config.APP_NAME}-{config.ENV}-stepfunction",
     lambda_stack,
     env=env_details
 )
 
-step_function_stack = StepFunctionStack(
+arns = step_function_stack.get_arns(env_details)
+
+integrate_to_lambdas(
+    lambda_stack.lambdas["trigger_cogify_lambda"],
+    lambda_stack.lambdas["trigger_ingest_lambda"],
+    arns[0],
+    arns[1],
+)
+
+queue_stack = QueueStack(
     app,
-    f"{config.APP_NAME}-stepfunction",
+    f"{config.APP_NAME}-{config.ENV}-queue",
     lambda_stack,
     env=env_details
 )
+
 
 app.synth()
--- dataset-workflows/simple-ingest/cdk/step_function_stack.py
+++ dataset-workflows/simple-ingest/cdk/step_function_stack.py
@@ -15,42 +15,46 @@ class StepFunctionStack(core.Stack):
     ):
         super().__init__(app, construct_id, **kwargs)
 
+        self.construct_id = construct_id
         lambdas = lambda_stack.lambdas
         s3_discovery_lambda = lambdas["s3_discovery_lambda"]
         cmr_discovery_lambda = lambdas["cmr_discovery_lambda"]
         cogify_lambda = lambdas["cogify_lambda"]
-        generate_stac_item_lambda = lambdas["generate_stac_item_lambda"]
-        db_write_lambda = lambdas["db_write_lambda"]
+        build_ndjson_lambda = lambdas["build_ndjson_lambda"]
+        pgstac_loader_lambda = lambdas["pgstac_loader_lambda"]
+        cogify_or_not_lambda = lambdas["cogify_or_not_lambda"]
 
-        cmr_discover_task = self._lambda_task("CMR Discover Task", cmr_discovery_lambda)
-        s3_discover_task = self._lambda_task("S3 Discover Task", s3_discovery_lambda)
+        cogify_or_not_task = self._lambda_task("Cogify Or Not Task", cogify_or_not_lambda, input_path="$.Payload")
+        cmr_discover_task = self._lambda_task("CMR Discover Task", cmr_discovery_lambda).next(cogify_or_not_task)
+        s3_discover_task = self._lambda_task("S3 Discover Task", s3_discovery_lambda).next(cogify_or_not_task)
         cogify_task = self._lambda_task("Cogify", cogify_lambda)
-        generate_stac_item_task = self._lambda_task("Generate STAC Item Task", generate_stac_item_lambda)
-        s3_db_write_task = self._lambda_task("DB Write task", db_write_lambda, input_path="$.Payload")
-
-        generate_and_write_stac_item_task = generate_stac_item_task.next(s3_db_write_task)
-        cogify = stepfunctions.Choice(self, "Cogify?")\
-            .when(stepfunctions.Condition.boolean_equals("$.cogify", True), cogify_task.next(generate_and_write_stac_item_task))\
-            .otherwise(generate_and_write_stac_item_task)
-        
-        map_cogify = stepfunctions.Map(
-            self,
-            "Map STAC Item Generate and Write",
-            max_concurrency=10,
-            items_path=stepfunctions.JsonPath.string_at("$.Payload"),
-        ).iterator(cogify)
-
-        queue = stepfunctions.Choice(self, "Queue?")\
-            .when(stepfunctions.Condition.boolean_equals("$.queue", True), stepfunctions.Succeed(self, "Queued."))\
-            .otherwise(map_cogify)
-
-        s3_wflow_definition = stepfunctions.Choice(self, "Discovery Choice (CMR or S3)")\
-            .when(stepfunctions.Condition.string_equals("$.discovery", "s3"), s3_discover_task.next(queue))\
-            .when(stepfunctions.Condition.string_equals("$.discovery", "cmr"), cmr_discover_task.next(queue))\
+        build_ndjson_task = self._lambda_task("Build Ndjson Task", build_ndjson_lambda)
+        pgstac_loader_task = self._lambda_task("Write to database Task", pgstac_loader_lambda, input_path="$.Payload")
+        discovery_workflow = stepfunctions.Choice(self, "Discovery Choice (CMR or S3)")\
+            .when(stepfunctions.Condition.string_equals("$.discovery", "s3"), s3_discover_task)\
+            .when(stepfunctions.Condition.string_equals("$.discovery", "cmr"), cmr_discover_task)\
             .otherwise(stepfunctions.Fail(self, "Discovery Type not supported"))
 
-        stepfunctions.StateMachine(
-            self, f"{construct_id}-state-machine", state_machine_name=f"{construct_id}state-machine" , definition=s3_wflow_definition
+        cogify_workflow = stepfunctions.Map(
+            self,
+            "Run concurrent cogifications",
+            max_concurrency=100,
+            items_path=stepfunctions.JsonPath.string_at("$.Payload"),
+        ).iterator(cogify_task)
+
+        ingest_and_publish_workflow = build_ndjson_task.next(pgstac_loader_task)
+
+        self._step_functions = {}
+        self._step_functions["discovery"] = stepfunctions.StateMachine(
+            self, f"{construct_id}-discover-sf", state_machine_name=f"{construct_id}-discover", definition=discovery_workflow
+        )
+
+        self._step_functions["cogify"] = stepfunctions.StateMachine(
+            self, f"{construct_id}-cogify-sf", state_machine_name=f"{construct_id}-cogify", definition=cogify_workflow
+        )
+
+        self._step_functions["publication"] = stepfunctions.StateMachine(
+            self, f"{construct_id}-publication-sf", state_machine_name=f"{construct_id}-publication", definition=ingest_and_publish_workflow
         )
 
     def _lambda_task(self, name, lambda_function, input_path=None):
@@ -59,4 +63,15 @@ class StepFunctionStack(core.Stack):
             name,
             lambda_function=lambda_function,
             input_path=input_path,
+        )
+
+    @property
+    def state_machines(self):
+        return self._step_functions
+
+    def get_arns(self, env_vars):
+        base_str = f"arn:aws:states:{env_vars.region}:{env_vars.account}:stateMachine:"
+        return (
+            f"{base_str}{self.construct_id}-cogify",
+            f"{base_str}{self.construct_id}-publication"
         )
