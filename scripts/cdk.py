e7a7e99be1c0fb1c9f571880cce53f4cef286864
“slesaad” <slesaad@gmail.com>
2022-06-02 15:02:37 -0500

    Add poetry script to cdk deploy and destroy

---
+++ scripts/cdk.py
@@ -0,0 +1,10 @@
+import os
+
+
+def deploy():
+    os.chdir('deploy')
+    os.system('cdk deploy --all --require-approval never')
+
+def destroy():
+    os.chdir('deploy')
+    os.system('cdk destroy --all --require-approval never')
