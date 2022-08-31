b87d080a5cddfd1e477c5418aa380ebf9314bf08
“slesaad” <slesaad@gmail.com>
2022-08-31 09:47:27 -0500

    Update insert-collection script to load using pypgstac
    (Temporary until stac-ingestor has a ingest collection endpoint)

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,8 @@ from sys import argv
 import functools
 import glob
 import os
+import base64
+import json
 
 import boto3
 
@@ -24,11 +26,13 @@ def arguments():
         return
     return argv[1:]
 
+
 def data_files(data, data_path):
     files = []
     for item in data:
         files.extend(glob.glob(os.path.join(data_path,  f"{item}*.json")))
     return files
+
 
 def args_handler(func):
     @functools.wraps(func)
@@ -36,3 +40,32 @@ def args_handler(func):
         internal_args = arguments()
         func(internal_args)
     return prep_args
+
+
+def get_secret(secret_name: str) -> None:
+    """Retrieve secrets from AWS Secrets Manager
+
+    Args:
+        secret_name (str): name of aws secrets manager secret containing database connection secrets
+        profile_name (str, optional): optional name of aws profile for use in debugger only
+
+    Returns:
+        secrets (dict): decrypted secrets in dict
+    """
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session(region_name="us-west-2")
+    client = session.client(service_name="secretsmanager")
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+
+    # Decrypts secret using the associated KMS key.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if "SecretString" in get_secret_value_response:
+        return json.loads(get_secret_value_response["SecretString"])
+    else:
+        return json.loads(base64.b64decode(get_secret_value_response["SecretBinary"]))
