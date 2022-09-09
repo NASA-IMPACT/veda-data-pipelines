7f7d29b21600e3eb72d7c58b22e0d0e4a8b4a8c8
Anthony Lukach <anthonylukach@gmail.com>
2022-09-09 12:59:35 -0700

    Simplify tests

--- lambdas/build-stac/tests/test_handler.py
+++ lambdas/build-stac/tests/test_handler.py
@@ -39,70 +39,55 @@ def override_registry(
 
 def test_routing_regex_event():
     """
-    Ensure that the system properly calls the generate_stac_regexevent when regex-style
-    events are provided.
+    Ensure that the system properly identifies, classifies, and routes regex-style events.
     """
-    expected_stac_item = build_mock_stac_item({"mock": "STAC Item 1"})
-    unexpected_stac_item = build_mock_stac_item({"mock": "STAC Item 2"})
+    regex_event = {
+        "collection": "test-collection",
+        "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
+        "granule_id": None,
+        "datetime_range": None,
+        "start_datetime": None,
+        "end_datetime": None,
+    }
+
     with override_registry(
         stac.generate_stac,
         events.RegexEvent,
-        MagicMock(return_value=expected_stac_item),
-    ), override_registry(
+        MagicMock(return_value=build_mock_stac_item({"mock": "STAC Item 1"})),
+    ) as called_mock, override_registry(
         stac.generate_stac,
         events.CmrEvent,
-        MagicMock(return_value=unexpected_stac_item),
+        MagicMock(),
     ) as not_called_mock:
-        output = handler.handler(
-            {
-                "collection": "test-collection",
-                "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-                "granule_id": None,
-                "datetime_range": None,
-                "start_datetime": None,
-                "end_datetime": None,
-            },
-            None,
-        )
+        handler.handler(regex_event, None)
 
+    called_mock.assert_called_once_with(events.RegexEvent.parse_obj(regex_event))
     assert not not_called_mock.call_count
-    expected_stac_item.to_dict.assert_called_once_with()
-    assert "stac_item" in output
-    created_stac_item = output["stac_item"]
-    assert created_stac_item == expected_stac_item.to_dict.return_value
 
 
 def test_routing_cmr_event():
     """
-    Ensure that the system properly calls the generate_stac_cmrevent when CMR-style
-    events are provided.
+    Ensure that the system properly identifies, classifies, and routes CMR-style events.
     """
-    expected_stac_item = build_mock_stac_item({"mock": "STAC Item 1"})
-    unexpected_stac_item = build_mock_stac_item({"mock": "STAC Item 2"})
+    cmr_event = {
+        "collection": "test-collection",
+        "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
+        "granule_id": "test-granule",
+    }
 
     with override_registry(
         stac.generate_stac,
         events.CmrEvent,
-        MagicMock(return_value=expected_stac_item),
-    ), override_registry(
+        MagicMock(return_value=build_mock_stac_item({"mock": "STAC Item 1"})),
+    ) as called_mock, override_registry(
         stac.generate_stac,
         events.RegexEvent,
-        MagicMock(return_value=unexpected_stac_item),
+        MagicMock(),
     ) as not_called_mock:
-        output = handler.handler(
-            {
-                "collection": "test-collection",
-                "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-                "granule_id": "test-granule",
-            },
-            None,
-        )
+        handler.handler(cmr_event, None)
 
+    called_mock.assert_called_once_with(events.CmrEvent.parse_obj(cmr_event))
     assert not not_called_mock.call_count
-    expected_stac_item.to_dict.assert_called_once_with()
-    assert "stac_item" in output
-    created_stac_item = output["stac_item"]
-    assert created_stac_item == expected_stac_item.to_dict.return_value
 
 
 @pytest.mark.parametrize(
