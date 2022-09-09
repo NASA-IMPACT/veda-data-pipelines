9dbf999a53c095f51e3b773b2b5c62b0c74d919c
Anthony Lukach <anthonylukach@gmail.com>
2022-09-08 22:51:21 -0700

    Update tests

--- lambdas/build-stac/tests/test_handler.py
+++ lambdas/build-stac/tests/test_handler.py
@@ -1,5 +1,5 @@
 import contextlib
-from typing import TYPE_CHECKING, Any, Type
+from typing import TYPE_CHECKING, Any, Dict, Type
 from unittest.mock import MagicMock, Mock
 from pydantic import ValidationError
 
@@ -11,6 +11,12 @@ from utils import stac, events
 
 if TYPE_CHECKING:
     from functools import _SingleDispatchCallable
+
+
+def build_mock_stac_item(item: Dict[str, Any]) -> MagicMock:
+    expected_stac_item = MagicMock(spec=Item)
+    expected_stac_item.to_dict.return_value = item
+    return expected_stac_item
 
 
 @contextlib.contextmanager
@@ -33,19 +39,21 @@ def test_routing_regex_event():
     Ensure that the system properly calls the generate_stac_regexevent when regex-style
     events are provided.
     """
-    mock_stac_dict = {"mock": "STAC Item"}
-    mock_stac_item = MagicMock(spec=Item)
-    mock_stac_item.to_dict.return_value = mock_stac_dict
+    expected_stac_item = build_mock_stac_item({"mock": "STAC Item 1"})
+    unexpected_stac_item = build_mock_stac_item({"mock": "STAC Item 2"})
     with override_registry(
-        stac.generate_stac, events.RegexEvent, MagicMock(return_value=mock_stac_item)
+        stac.generate_stac,
+        events.RegexEvent,
+        MagicMock(return_value=expected_stac_item),
     ), override_registry(
-        stac.generate_stac, events.CmrEvent, MagicMock()
+        stac.generate_stac,
+        events.CmrEvent,
+        MagicMock(return_value=unexpected_stac_item),
     ) as not_called_mock:
         output = handler.handler(
             {
                 "collection": "test-collection",
                 "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-                "filename_regex": "^.*.tif$",
                 "granule_id": None,
                 "datetime_range": None,
                 "start_datetime": None,
@@ -54,11 +62,11 @@ def test_routing_regex_event():
             None,
         )
 
-    mock_stac_item.to_dict.assert_called_once_with()
+    assert not not_called_mock.call_count
+    expected_stac_item.to_dict.assert_called_once_with()
     assert "stac_item" in output
     created_stac_item = output["stac_item"]
-    assert created_stac_item == mock_stac_dict
-    assert not not_called_mock.call_count
+    assert created_stac_item == expected_stac_item.to_dict.return_value
 
 
 def test_routing_cmr_event():
@@ -66,13 +74,17 @@ def test_routing_cmr_event():
     Ensure that the system properly calls the generate_stac_cmrevent when CMR-style
     events are provided.
     """
-    mock_stac_dict = {"mock": "STAC Item"}
-    mock_stac_item = MagicMock(spec=Item)
-    mock_stac_item.to_dict.return_value = mock_stac_dict
+    expected_stac_item = build_mock_stac_item({"mock": "STAC Item"})
+    unexpected_stac_item = build_mock_stac_item({"mock": "STAC Item"})
+
     with override_registry(
-        stac.generate_stac, events.CmrEvent, MagicMock(return_value=mock_stac_item)
+        stac.generate_stac,
+        events.CmrEvent,
+        MagicMock(return_value=expected_stac_item),
     ), override_registry(
-        stac.generate_stac, events.RegexEvent, MagicMock()
+        stac.generate_stac,
+        events.RegexEvent,
+        MagicMock(return_value=unexpected_stac_item),
     ) as not_called_mock:
         output = handler.handler(
             {
@@ -83,21 +95,16 @@ def test_routing_cmr_event():
             None,
         )
 
-    mock_stac_item.to_dict.assert_called_once_with()
+    assert not not_called_mock.call_count
+    expected_stac_item.to_dict.assert_called_once_with()
     assert "stac_item" in output
     created_stac_item = output["stac_item"]
-    assert created_stac_item == mock_stac_dict
-    assert not not_called_mock.call_count
+    assert created_stac_item == expected_stac_item.to_dict.return_value
 
 
 @pytest.mark.parametrize(
     "bad_event",
-    [
-        {
-            "collection": "test-collection",
-            "s3_filename": "s3://test-bucket/delivery/BMHD_Maria_Stages/70001_BeforeMaria_Stage0_2017-07-21.tif",
-        }
-    ],
+    [{"collection": "test-collection"}],
 )
 def test_routing_unexpected_event(bad_event):
     """
