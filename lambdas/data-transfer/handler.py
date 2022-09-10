79c101c1f704e5d172f66c8788f4102b76119aac
Anthony Lukach <anthonylukach@gmail.com>
2022-09-09 23:27:08 -0700

    Rework url parser to fix bugs:
    1. .strip() used incorrectly
    2. path and key could be equal if file at root of bucket
    
    -
    
    Also, simplify temporary file

--- lambdas/data-transfer/handler.py
+++ lambdas/data-transfer/handler.py
@@ -4,12 +4,6 @@ import boto3
 from botocore.errorfactory import ClientError
 
 
-def _parse_s3_object(s3_object):
-    split = s3_object.strip("s3://").split("/")
-    bucket, path, key = split[0], "/".join(split[1:-1]), split[-1]
-    return bucket, path, key
-
-
 def assume_role(role_arn, session_name):
     sts = boto3.client("sts")
     creds = sts.assume_role(
@@ -37,10 +31,12 @@ def handler(event, context):
     for object in event:
         if not object.get("upload"):
             continue
-        bucket, path, name = _parse_s3_object(object["s3_filename"])
-        tmp_filename = f"/tmp/{name}"
+        url = urllib.parse.urlparse(object["s3_filename"])
+        src_bucket = url.hostname
+        src_key = url.path.strip("/")
+        filename = src_key.split("/")[-1]
 
-        target_key = f"{object.get('collection')}/{name}"
+        target_key = f"{object.get('collection')}/{filename}"
         target_url = f"s3://{TARGET_BUCKET}/{target_key}"
 
         # Check if the corresponding object exists in the target bucket
@@ -49,11 +45,10 @@ def handler(event, context):
         except ClientError:
             try:
                 # Not found
-                source_s3.download_file(bucket, f"{path}/{name}", tmp_filename)
-                target_s3.upload_file(tmp_filename, TARGET_BUCKET, target_key)
-                # Clean up the data
-                if os.path.exists(tmp_filename):
-                    os.remove(tmp_filename)
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    tmp_filename = f"{tmp_dir}/{filename}"
+                    source_s3.download_file(src_bucket, src_key, tmp_filename)
+                    target_s3.upload_file(tmp_filename, TARGET_BUCKET, target_key)
             except:
                 print(f"Failed while trying to upload missing file at s3://{bucket}/{target_key}.")
                 raise
