8702a8cb7f40f91c4ae221a7649d504372b9b6c6
Anthony Lukach <anthonylukach@gmail.com>
2022-09-12 16:06:28 -0600

    Add testing for data-transfer lambda (#185)
    * Add actions code to test data-transfer lambda
    
    * Split testing workflows
    
    * Mv target bucket into handler for easier mocking
    
    * Rework url parser to fix bugs:
    
    1. .strip() used incorrectly
    2. path and key could be equal if file at root of bucket
    
    -
    
    Also, simplify temporary file
    
    * Format
    
    * Add test
    
    * Add newline
    
    * Use pip reather than poetry
    
    * Revert "Use pip reather than poetry"
    
    This reverts commit 450090ca8a63389ea7d501952f7e91562e030eb8.
    
    * Attempt to fix install issue

--- lambdas/data-transfer/handler.py
+++ lambdas/data-transfer/handler.py
@@ -1,27 +1,23 @@
 import os
+import urllib.parse
+import tempfile
 
 import boto3
 from botocore.errorfactory import ClientError
 
 
-TARGET_BUCKET = os.environ["BUCKET"]
-
-
-def _parse_s3_object(s3_object):
-    split = s3_object.strip("s3://").split("/")
-    bucket, path, key = split[0], "/".join(split[1:-1]), split[-1]
-    return bucket, path, key
-
-
 def assume_role(role_arn, session_name):
     sts = boto3.client("sts")
     creds = sts.assume_role(
-        RoleArn=role_arn, RoleSessionName=session_name,
+        RoleArn=role_arn,
+        RoleSessionName=session_name,
     )
     return creds["Credentials"]
 
 
 def handler(event, context):
+    TARGET_BUCKET = os.environ["BUCKET"]
+
     kwargs = {}
     if role_arn := os.environ.get("EXTERNAL_ROLE_ARN"):
         creds = assume_role(role_arn, "veda-data-pipelines_data-transfer")
@@ -31,17 +27,18 @@ def handler(event, context):
             "aws_session_token": creds["SessionToken"],
         }
     source_s3 = boto3.client("s3")
-    target_s3 = boto3.client(
-        "s3",
-        **kwargs
-    )
+    target_s3 = boto3.client("s3", **kwargs)
+
     for object in event:
         if not object.get("upload"):
             continue
-        bucket, path, name = _parse_s3_object(object["s3_filename"])
-        tmp_filename = f"/tmp/{name}"
 
-        target_key = f"{object.get('collection')}/{name}"
+        url = urllib.parse.urlparse(object["s3_filename"])
+        src_bucket = url.hostname
+        src_key = url.path.strip("/")
+        filename = src_key.split("/")[-1]
+
+        target_key = f"{object.get('collection')}/{filename}"
         target_url = f"s3://{TARGET_BUCKET}/{target_key}"
 
         # Check if the corresponding object exists in the target bucket
@@ -50,13 +47,15 @@ def handler(event, context):
         except ClientError:
             try:
                 # Not found
-                source_s3.download_file(bucket, f"{path}/{name}", tmp_filename)
-                target_s3.upload_file(tmp_filename, TARGET_BUCKET, target_key)
-                # Clean up the data
-                if os.path.exists(tmp_filename):
-                    os.remove(tmp_filename)
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    tmp_filename = f"{tmp_dir}/{filename}"
+                    source_s3.download_file(src_bucket, src_key, tmp_filename)
+                    target_s3.upload_file(tmp_filename, TARGET_BUCKET, target_key)
             except:
-                print(f"Failed while trying to upload missing file at s3://{bucket}/{target_key}.")
+                print(
+                    "Failed while trying to upload file from "
+                    f"s3://{src_bucket}/{src_key} to s3://{TARGET_BUCKET}/{target_key}."
+                )
                 raise
 
         object["s3_filename"] = target_url
