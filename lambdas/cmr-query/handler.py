f50285db5720d6648989fa40ae3ba576ea349a27
Aimee Barciauskas <aimee@developmentseed.org>
2022-12-02 08:19:19 -0800

    Clean up pagination in CMR handler

--- lambdas/cmr-query/handler.py
+++ lambdas/cmr-query/handler.py
@@ -1,8 +1,15 @@
+import os
 import re
 import json
 import datetime as dt
 
 import requests
+
+def get_cmr_granules_endpoint(event):
+    default_cmr_api_url = "https://cmr.earthdata.nasa.gov"
+    cmr_api_url = event.get('cmr_api_url', os.environ.get('CMR_API_URL', default_cmr_api_url))
+    cmr_granules_search_url = f"{cmr_api_url}/search/granules.json"
+    return cmr_granules_search_url
 
 def handler(event, context):
     """
@@ -14,19 +21,15 @@ def handler(event, context):
     temporal = event.get("temporal", ["1000-01-01T00:00:00Z", "3000-01-01T23:59:59Z"])
     startdate = dt.datetime.strptime(temporal[0], "%Y-%m-%dT%H:%M:%SZ")
     enddate = dt.datetime.strptime(temporal[1], "%Y-%m-%dT%H:%M:%SZ")
-    page = event.get('start_after')
+    page = event.get('start_after', 1)
     limit = event.get('limit', 100)
 
-    cmr_api_url = f"https://cmr.maap-project.org/search/granules.json"
-    search_endpoint = f"{cmr_api_url}?short_name={collection}&version={version}" + \
+    search_endpoint = f"{get_cmr_granules_endpoint(event)}?short_name={collection}&version={version}" + \
       f"&temporal[]={temporal[0]},{temporal[1]}&page_size={limit}"
+    search_endpoint = f"{search_endpoint}&page_num={page}"
     print(f"Discovering data from {search_endpoint}")
-    if page:
-        search_endpoint = f"{search_endpoint}&page_num={page}"
-        page +=1
-    # SEarch after is throwing 500 error in CMR
-    # headers = {'CMR-Search-After': str(event.get('search_after'))}
     response = requests.get(search_endpoint)
+
     if response.status_code != 200:
         print(f"Got an error from CMR: {response.status_code} - {response.text}")
         return
@@ -34,6 +37,18 @@ def handler(event, context):
         hits = response.headers['CMR-Hits']
         print(f"Got {hits} from CMR")
         granules = json.loads(response.text)['feed']['entry']
+        print(f"Got {len(granules)} to insert")
+        # Decide if we should continue after this page
+        # Start paging if there are more hits than the limit
+        # Stop paging when there are no more results to return
+        if len(granules) > 0 and int(hits) > limit*page:
+            print(f"Got {int(hits)} which is greater than {limit*page}")
+            print(f"Returning next page {event['start_after']}")
+            page += 1
+            event['start_after'] = page
+        else:
+            event.pop('start_after')
+
     granules_to_insert = []
     for granule in granules:
         file_obj = {}
@@ -56,9 +71,6 @@ def handler(event, context):
                             file_obj[key] = value
         granules_to_insert.append(file_obj)
 
-
-
-    print(f"Returning {len(granules_to_insert)} granules to insert")
     # Useful for testing locally with build-stac/handler.py
     # print(json.dumps(granules_to_insert[0], indent=2))
     return_obj = {
@@ -66,12 +78,7 @@ def handler(event, context):
         "cogify": event.get("cogify", False),
         "objects": granules_to_insert
     }
-    # print(json.dumps(granules_to_insert[0], indent=2))
-    if len(granules_to_insert) > 0:
-        return_obj['start_after'] = page or 2
-        print(f"returning next page {return_obj['start_after']}")
-    else:
-        return_obj.pop('start_after')
+    print(event)
     return return_obj
 
 
@@ -82,8 +89,9 @@ if __name__ == "__main__":
         "temporal": ["2021-01-01T00:00:00Z", "2021-12-31T23:59:59Z"],
         "version": "1",
         "discovery": "cmr",
+        "cmr_api_url": "https://cmr.maap-project.org",
         "asset_name": "data",
         "asset_roles": ["data"],
-        "start_after": 1
+        "start_after": 4
     }
     handler(sample_event, {})
